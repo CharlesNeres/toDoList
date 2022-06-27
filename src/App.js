d96db5d395c78874225db65aebe827c1718561c3
Charles Júnior <charlesbritoneresjr@gmail.com>
2022-06-27 16:30:58 +0800

    implemented background component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+import background from './images/bg-mobile-light.jpg';
+import BackgroundCss from "./components/background/BackgroundCss";
+
 import './App.css';
 
 class App extends React.Component{
     render(){
         return(
-            <main>
-                
-            </main>
+            <BackgroundCss>
+                <header>
+                    <img src={background}/>
+                </header>                
+            </BackgroundCss>
         )
     }
 }
