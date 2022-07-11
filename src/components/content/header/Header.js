b250e1d549d18bbcecea719228116f57aca9bcce
Charles Júnior <charlesbritoneresjr@gmail.com>
2022-07-11 23:26:28 +0800

    enlarged font and icon of header component

--- src/components/content/header/Header.js
+++ src/components/content/header/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HeaderCss from "./HeaderCss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
 
 class Header extends React.Component{
     render(){
         return(
             <HeaderCss>
                 <h1>todo</h1>
-                <span>teste</span>
+                <FontAwesomeIcon className="icon-day-night" icon={faMoon}/>
             </HeaderCss>
         )
     }
--- src/components/content/header/HeaderCss.js
+++ src/components/content/header/HeaderCss.js
@@ -2,15 +2,18 @@ import styled from "styled-components";
 
 const HeaderCss = styled.header`
     overflow: hidden;
+    color: #fff;
 
     h1{
         float: left;
         text-transform: uppercase;
-        font-size: 90%;
+        font-size: 150%;
+        letter-spacing: .4em;
     }
 
-    span{
+    .icon-day-night{
         float: right;
+        font-size: 140%;
     }
 `;
 
